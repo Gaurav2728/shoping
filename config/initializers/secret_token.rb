e5b70857eae0236a327389b17b51878106812bb4
Gaurav Sharma <gaurav2728@gmail.com>
2012-12-05 15:11:21 +0530

    Depot shoping

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shoping::Application.config.secret_token = '7ffcbfbc573d32efb6f953c57951bfd9636034fb867943148a77c5dcb0eaff3f6077f30eeb0aa22c8920c83289691355da2b28322b1db0b0cdf2f1d1a19bfe16'
