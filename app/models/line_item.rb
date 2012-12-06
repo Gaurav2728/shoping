24cf4268097e6ac70370afc92a0288e4658c956c
Gaurav Sharma <gaurav2728@gmail.com>
2012-12-06 18:16:47 +0530

    shoping

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -3,4 +3,9 @@ class LineItem < ActiveRecord::Base
   #attr_accessible :product # solution of-> ActiveModel::MassAssignmentSecurity::Error in LineItemsController#create
   belongs_to :product
 	belongs_to :cart
+
+	def total_price
+		product.price * quantity
+	end
+
 end
