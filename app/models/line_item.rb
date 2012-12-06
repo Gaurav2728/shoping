ef1a8012f3b528af05d38aaa38c0c5a736ac6b78
Gaurav Sharma <gaurav2728@gmail.com>
2012-12-06 14:07:29 +0530

    cart

---
+++ app/models/line_item.rb
@@ -0,0 +1,6 @@
+class LineItem < ActiveRecord::Base
+  attr_accessible :cart_id, :product_id
+  #attr_accessible :product # solution of-> ActiveModel::MassAssignmentSecurity::Error in LineItemsController#create
+  belongs_to :product
+	belongs_to :cart
+end
