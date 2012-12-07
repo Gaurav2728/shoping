ce580b7a8bed2ece7e1b4cc651844d853074b63a
Gaurav Sharma <gaurav2728@gmail.com>
2012-12-07 19:12:16 +0530

    new layout

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -49,8 +49,8 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.save
         #format.html { redirect_to @line_item, notice: 'Line item was successfully created.' }
-        format.html { redirect_to @line_item.cart }
-
+        format.html { redirect_to store_url }
+        format.js   { @current_item = @line_item }  # javascript file create.js.erb
         format.json { render json: @line_item, status: :created, location: @line_item }
       else
         format.html { render action: "new" }
