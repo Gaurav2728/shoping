8914986e8277930377029e58570d80fa5dd12071
Gaurav Sharma <gaurav2728@gmail.com>
2012-12-12 15:41:13 +0530

    mailer

---
+++ app/mailers/order_notifier.rb
@@ -0,0 +1,26 @@
+class OrderNotifier < ActionMailer::Base
+  default from: "gaurav2728@gmail.com"
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_notifier.received.subject
+  #
+  def received(order)
+    @order = order
+
+    mail to: order.email, subject: 'Book Shoping Order Confirmation'
+
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_notifier.shipped.subject
+  #
+  def shipped(order)
+    @order = order
+
+    mail to: order.email, subject: 'Book Shoping Order Shipped'
+  end
+end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,8 +13,21 @@ Shoping::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
+  # care if the mailer can't send
+  config.action_mailer.raise_delivery_errors = true
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:        "smtp.gmail.com",
+    port:           587,
+    domain:         "BookShoping.com",
+    authentication: "plain",
+    user_name:      "gaurav2728",
+    password:       "1031#Pratap2728@@",
+    enable_starttls_auto: true
+  }
+
+
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
