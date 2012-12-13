618f3cb755515fc55ee418f48194fd2d8065e313
Gaurav Sharma <gaurav2728@gmail.com>
2012-12-13 16:51:28 +0530

    env

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,12 @@
 class Order < ActiveRecord::Base
   PAYMENT_TYPES = [ "Check", "Credit card", "Purchase order" ] # it should always before validation lines.
-
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
   attr_accessible :address, :email, :name, :pay_type
   validates :name, :address, :email, presence: true
+  validates :email, presence: true,
+                    format: { with: VALID_EMAIL_REGEX }
+
   validates :pay_type, inclusion: PAYMENT_TYPES
 
   has_many :line_items, dependent: :destroy
